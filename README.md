2df9d113338142e8e18a029fe88aa595bc8f37b0
dracarystian <dcastiblanco14@gmail.com>
2024-05-09 14:13:38 -0500

    bd funcionando

---
+++ README.md
@@ -0,0 +1,13 @@
+# Prueba Tecnica para Datos Y Proyectos
+
+# Primera Parte:
+
+- Para la creacion de la base de datos use MySQL a traves de WorkBench y para hacer la conexion usa la extencion *pymysql* 
+
+- Se añade la funcion para poder hacer las consultas directamente desde python añadiendo un cursor para que permita mostrar las consultas en consola.
+
+# Segunda Parte
+
+A partir de la base de datos pude crear los endpoints necesarios para obtener los datos de 'cliente', borrar y modificar. 
+
+Para esta parte use Flask, y jsonfy para convertir los datos a json, es de mis primeras veces usando esta tecnología, muy cómoda y facil de usar.
--- Server/connect.py
+++ Server/connect.py
@@ -4,9 +4,55 @@ import pymysql
 connection_bd = pymysql.connect(host='localhost',
                              user='root',
                              password='123456',
-                             database='pruebapyd')
+                             database='pruebapyd',
+                             cursorclass=pymysql.cursors.DictCursor)
 
-print(connection_bd)
+cursor = connection_bd.cursor()
 
+#Consulta a la primera pregunta del ejercicio:
+query = """
+            SELECT c.Nombre AS Nombre_Cliente, AVG(f.Valor) AS Promedio_Ventas
+            FROM Cliente c
+            LEFT JOIN Factura f ON c.Cedula = f.Cedula
+            WHERE YEAR(f.Fecha_factura) = 2006
+            AND c.Cedula NOT IN (
+                SELECT DISTINCT Cedula
+                FROM Factura
+                WHERE YEAR(Fecha_factura) = 2005
+                AND Cedula IS NOT NULL
+            )
+            GROUP BY c.Nombre;
+        """
+#Consulta a la segunda pregunta del ejercicio:
+
+#query = """
+#            WITH Ventas AS (
+#                SELECT c.Nombre AS Nombre_Cliente, SUM(f.Valor) AS Valor_Acumulado, YEAR(f.Fecha_factura) AS Anio
+#                FROM Cliente c
+#                JOIN Factura f ON c.Cedula = f.Cedula
+#                JOIN Ciudad ci ON c.Codigo_ciudad = ci.Codigo_ciudad
+#                JOIN Departamento d ON ci.Cod_departamento = d.Codigo_depar
+#                WHERE d.Nombre_depar = 'Departamento X'
+#                AND (MONTH(f.Fecha_factura) BETWEEN 1 AND 3)
+#                GROUP BY c.Nombre, YEAR(f.Fecha_factura)
+#            )
+#            SELECT Anio, Nombre_Cliente, Valor_Acumulado
+#            FROM (
+#               SELECT Anio, Nombre_Cliente, Valor_Acumulado,
+#                       ROW_NUMBER() OVER (PARTITION BY Anio ORDER BY Valor_Acumulado DESC) AS rn
+#                FROM Ventas
+#            ) AS RankedVentas
+#           WHERE rn = 1;
+#        """        
+
+cursor.execute(query)
+
+
+# Obtener los resultados de la consulta
+results = cursor.fetchall()
+
+# Mostrar los resultados
+for row in results:
+    print(row)
 
 
